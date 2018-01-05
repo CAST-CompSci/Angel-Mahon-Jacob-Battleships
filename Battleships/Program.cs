b03c513916ddc6996991160a292ec4a60de415d7
Hughes <15U-MHughes@pfa.education>
2018-01-05 10:13:09 +0000

    Changed "u" to be removed.

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -25,9 +25,7 @@ namespace ConsoleApp10
                 }
             }
 
-            string u = "0";
-
-            u = Console.ReadLine();
+            
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("    A   B   C   D   E   F   G   H   I   J   K   L");
             Console.WriteLine("  ┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓");
