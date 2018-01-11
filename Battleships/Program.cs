0126ac32fefd50e454b5f0c03ce6fc1ff7c0578a
Watson <15U-AWatson@pfa.education>
2018-01-11 14:17:18 +0000

    random ship length and correct direction

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -28,7 +28,7 @@ namespace ConsoleApp10
             }
 
                 Random generator4 = new Random();
-                int size = generator.Next(2,5);
+                int size = generator4.Next(2,5);
          
                 Random generator = new Random();
                 int rand = generator.Next(1, 12);
@@ -40,32 +40,46 @@ namespace ConsoleApp10
 
                 Random generator3 = new Random();
                 int rand3 = generator.Next(1,4);
-            int count = 1; 
-            while (count <= size)
-            {
+           
               if (rand3 == 1)
               {
-              rand = rand + 1;
-              hitMatrix[rand, rand2] = "#";
-            }
+                while (size > 0)
+                {
+                    rand = rand + 1;
+                    hitMatrix[rand, rand2] = "#";
+                    size = size - 1;
+                }
+              }
               else if  (rand3 == 2)
               {
-              rand = rand - 1;
-              hitMatrix[rand, rand2] = "#";
+                while (size > 0)
+                {
+                    rand = rand - 1;
+                    hitMatrix[rand, rand2] = "#";
+                    size = size - 1;
+                }
               }
               else if  (rand3 == 3)
               {
-               rand2 = rand2 + 1;
-               hitMatrix[rand, rand2] = "#";
+                while (size > 0)
+                {
+                    rand2 = rand2 + 1;
+                    hitMatrix[rand, rand2] = "#";
+                    size = size - 1;
+                }
               }
             
               else if (rand3 == 4)
               {
-               rand2 = rand2 - 1;
-               hitMatrix[rand, rand2] = "#";
+                while (size > 0)
+                {
+                    rand2 = rand2 - 1;
+                    hitMatrix[rand, rand2] = "#";
+                    size = size - 1;
+                }
               }
 
-            }
+            
 
 
             Console.OutputEncoding = System.Text.Encoding.UTF8;
