156a5acc658c310b03b9f2d67d60c12d0e389df8
Watson <15U-AWatson@pfa.education>
2018-01-12 10:03:11 +0000

    Signed-off-by: Watson <15U-AWatson@pfa.education>

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -8,14 +8,13 @@ namespace ConsoleApp10
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             //Console.ForegroundColor = ConsoleColor.Blue; 
             //Console.BackgroundColor = ConsoleColor.White;
             //Console.Clear();
             //^Changes whole console colour and foreground colour now.
-
+            int size = 0;
             string[,] hitMatrix = new string[12, 12];
 
 
@@ -26,24 +25,31 @@ namespace ConsoleApp10
                     hitMatrix[i, j] = (" ");
                 }
             }
-            int p = 1;
-            while (p == 1)
-            {
-                Random generator4 = new Random();
-                int size = generator4.Next(2, 5);
 
+            int amountofbotes = 5;
+
+            do
+            {
                 Random generator = new Random();
-                int rand = generator.Next(1, 12);
+                int rand = generator.Next(0, 11);
 
                 Random generator2 = new Random();
-                int rand2 = generator.Next(1, 12);
+                int rand2 = generator.Next(0, 11);
 
                 hitMatrix[rand, rand2] = "#";
 
                 Random generator3 = new Random();
-                int rand3 = generator.Next(1, 4);
+                int direction = generator.Next(1, 4);
 
-                if (rand3 == 1)
+                //  Random generator4 = new Random();
+                //int size = generator.Next(2, 5);
+
+
+                size = amountofbotes;
+                Console.WriteLine("Size" + size);
+
+
+                if (direction == 1)
                 {
                     while (size > 0)
                     {
@@ -52,43 +58,40 @@ namespace ConsoleApp10
                         size = size - 1;
                     }
                 }
-                else if (rand3 == 2)
+                else if (direction == 2)
                 {
                     while (size > 0)
                     {
                         rand = rand - 1;
-                        
                         hitMatrix[rand, rand2] = "#";
                         size = size - 1;
                     }
                 }
-                else if (rand3 == 3)
+                else if (direction == 3)
                 {
                     while (size > 0)
                     {
                         rand2 = rand2 + 1;
-                        
                         hitMatrix[rand, rand2] = "#";
                         size = size - 1;
                     }
                 }
 
-                else if (rand3 == 4)
+                else if (direction == 4)
                 {
                     while (size > 0)
                     {
-
-
                         rand2 = rand2 - 1;
-                        
                         hitMatrix[rand, rand2] = "#";
                         size = size - 1;
 
                     }
                 }
 
+                amountofbotes = amountofbotes - 1;
+                Console.WriteLine("amount of boats" + amountofbotes);
 
-
+            } while (amountofbotes >= 0);
 
                 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
@@ -127,7 +130,7 @@ namespace ConsoleApp10
 
                 }
                 Console.Read();
-            }
+            
         }
     }
 }
